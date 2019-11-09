81ec466491ee325969c3488c18cfb87d91eb999c
Jake Vasilakes <jvasilakes@gmail.com>
2019-11-09 17:44:25 -0600

    Make flake8 happy

--- combat_simulator/__init__.py
+++ combat_simulator/__init__.py
@@ -3,3 +3,6 @@ from .token import Token, Character
 from .player import Player
 from .grid import Grid
 from .encounter import Team, Encounter
+
+__all__ = ["Engine", "Token", "Character",
+           "Player", "Grid", "Team", "Encounter"]
--- tests/context.py
+++ tests/context.py
@@ -4,4 +4,4 @@ import sys
 curdir = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(curdir, ".."))
 
-import combat_simulator
+import combat_simulator  # noqa
