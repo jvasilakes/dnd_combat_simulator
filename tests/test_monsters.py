077c049342b9e79a98ef1c223e4ea3faf46052ff
Jake Vasilakes <vasil024@umn.edu>
2019-12-26 16:33:00 -0600

    Update filepath. lint setup.py.

--- tests/test_monsters.py
+++ tests/test_monsters.py
@@ -10,7 +10,8 @@ curdir = os.path.dirname(__file__)
 
 
 def test_load_monsters():
-    monster_file = os.path.join(curdir, "../assets/5e_SRD_monsters.jsonl")
+    monster_file = os.path.join(curdir,
+                                "../assets/5e_SRD_monsters_formatted.jsonl")
     monster_data = [json.loads(line) for line in open(monster_file)]
     for md in monster_data:
         monster = Character(**md)
@@ -19,7 +20,8 @@ def test_load_monsters():
 
 def test_attack_damage():
     player = Player(name="Test")
-    monster_file = os.path.join(curdir, "../assets/5e_SRD_monsters.jsonl")
+    monster_file = os.path.join(curdir,
+                                "../assets/5e_SRD_monsters_formatted.jsonl")
     monster_data = [json.loads(line) for line in open(monster_file)]
     for md in monster_data:
         monster = Character(**md)
